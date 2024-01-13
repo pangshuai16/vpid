1a5489c8722fca3e46bc9af6ed796171dc839e59
pangshuai16 <chauncytu@gmail.com>
2024-01-13 13:11:49 +0800

    基础实现

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,39 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use hidapi::HidApi;
+use serde::Serialize;
+
+#[derive(Debug, Serialize)]
+pub struct DeviceInfo {
+    product_name: String,
+    path: String,
+    vendor_id: u16,
+    product_id: u16,
+    vendor_name: String,
+}
+
+#[tauri::command]
+fn get_hid_devices() -> Result<Vec<DeviceInfo>, String> {
+    let api = HidApi::new().map_err(|err| format!("HID API error: {}", err))?;
+    let devices = api.device_list();
+
+    let devices: Vec<DeviceInfo> = devices
+        .map(|device| DeviceInfo {
+            product_name: device.product_string().unwrap().to_string(),
+            path: device.path().to_str().unwrap().to_string(),
+            vendor_id: device.vendor_id(),
+            product_id: device.product_id(),
+            vendor_name: device.manufacturer_string().unwrap_or_default().to_string(),
+        })
+        .collect();
+
+    Ok(devices)
+}
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![get_hid_devices])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
---
+++ src/App.svelte
@@ -0,0 +1,253 @@
+<script lang="ts">
+  import { invoke } from "@tauri-apps/api/tauri";
+  import { writeText } from "@tauri-apps/api/clipboard";
+  import { exit } from "@tauri-apps/api/process";
+  import { scale } from "svelte/transition";
+
+  interface RustDeviceInfo {
+    product_name: string;
+    path: string;
+    vendor_id: number;
+    product_id: number;
+    vendor_name: string;
+  }
+  interface DeviceInfo {
+    product_name: string;
+    path: string;
+    vendor_id: string;
+    product_id: string;
+    vendor_name: string;
+  }
+  let devices: DeviceInfo[] = [];
+  let baseList: DeviceInfo[] = [];
+  let addList: DeviceInfo[] = [];
+  let removeList: DeviceInfo[] = [];
+  let toastMsg = "";
+  let msgTimer: number | null;
+
+  getDevices();
+
+  async function getDevices() {
+    devices = (<RustDeviceInfo[]>await invoke("get_hid_devices"))
+      .map((item) => ({
+        ...item,
+        product_id: item.product_id
+          .toString(16)
+          .padStart(4, "0")
+          .toLocaleUpperCase(),
+        vendor_id: item.vendor_id
+          .toString(16)
+          .padStart(4, "0")
+          .toLocaleUpperCase(),
+      }))
+      .sort((a, b) => a.path.localeCompare(b.path));
+
+    if (baseList.length > 0) {
+      getAddlist();
+      getRemoveList();
+    } else {
+      saveBaseList();
+    }
+  }
+
+  function getAddlist() {
+    const paths = baseList.map((i) => i.path);
+    addList = devices.filter((i) => !paths.includes(i.path));
+  }
+  function getRemoveList() {
+    const paths = devices.map((i) => i.path);
+    removeList = baseList.filter((i) => !paths.includes(i.path));
+  }
+  async function saveBaseList() {
+    if (devices.length === 0) {
+      await getDevices();
+    } else {
+      baseList = devices;
+      addList = [];
+      removeList = [];
+      message("当前列表已设为新增和移除的对比基准");
+    }
+  }
+
+  function clean() {
+    removeList = addList = baseList = devices = [];
+  }
+
+  function copyItem(row: DeviceInfo) {
+    writeText(JSON.stringify(row))
+      .then(() => {
+        message(`${row.vendor_id}:${row.product_id} 复制成功`);
+      })
+      .catch(() => {
+        message("复制失败");
+      });
+  }
+  function copyCurrentList() {
+    writeText(JSON.stringify(devices))
+      .then(() => {
+        message("复制当前设备列表，成功！");
+      })
+      .catch(() => {
+        message("复制当前设备列表，失败");
+      });
+  }
+  function close() {
+    exit();
+  }
+
+  async function message(msg: string) {
+    if (msgTimer) {
+      toastMsg = "";
+      clearTimeout(msgTimer);
+      await sleep(100);
+    }
+    toastMsg = msg;
+
+    msgTimer = setTimeout(() => {
+      toastMsg = "";
+    }, 2500);
+  }
+
+  function sleep(ms: number): Promise<void> {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+</script>
+
+<main class="container">
+  <div class="box1">
+    <h3 class="lsit-title">当前设备列表</h3>
+    <div class="list-outer">
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">设备</th>
+            <th scope="col">VID</th>
+            <th scope="col">PID</th>
+          </tr>
+        </thead>
+        <tbody>
+          {#each devices as item}
+            <tr on:click={() => copyItem(item)}>
+              <td>{item.product_name}</td>
+              <td>{item.vendor_id}</td>
+              <td>{item.product_id}</td>
+            </tr>
+          {/each}
+        </tbody>
+      </table>
+    </div>
+  </div>
+  <div class="box2">
+    <h3 class="lsit-title">新增的设备</h3>
+    <div class="list-outer">
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">设备</th>
+            <th scope="col">VID</th>
+            <th scope="col">PID</th>
+          </tr>
+        </thead>
+        <tbody>
+          {#each addList as item}
+            <tr on:click={() => copyItem(item)}>
+              <td>{item.product_name}</td>
+              <td>{item.vendor_id}</td>
+              <td>{item.product_id}</td>
+            </tr>
+          {/each}
+        </tbody>
+      </table>
+    </div>
+    <h3 class="lsit-title">移除的设备</h3>
+    <div class="list-outer">
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">设备</th>
+            <th scope="col">VID</th>
+            <th scope="col">PID</th>
+          </tr>
+        </thead>
+        <tbody>
+          {#each removeList as item}
+            <tr on:click={() => copyItem(item)}>
+              <td>{item.product_name}</td>
+              <td>{item.vendor_id}</td>
+              <td>{item.product_id}</td>
+            </tr>
+          {/each}
+        </tbody>
+      </table>
+    </div>
+  </div>
+
+  <div class="box3">
+    <button on:click={getDevices}>刷新</button>
+    <button on:click={saveBaseList}> 设为基准</button>
+    <button on:click={copyCurrentList}>复制当前</button>
+    <button on:click={clean}>清空</button>
+    <button on:click={close}>退出</button>
+  </div>
+
+  {#if toastMsg}
+    <h5 class="toast" transition:scale={{ delay: 50, duration: 500 }}>
+      {toastMsg}
+    </h5>
+  {/if}
+</main>
+
+<style>
+  .container {
+    height: 100vh;
+    padding: 4px;
+    box-sizing: border-box;
+  }
+  .box1,
+  .box2,
+  .box3 {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+  }
+  .box1,
+  .box2 {
+    width: calc(50% - 60px);
+  }
+  .lsit-title {
+    margin: 4px;
+  }
+  .list-outer {
+    height: 100%;
+    overflow-y: auto;
+    border-radius: 8px;
+    box-shadow:
+      inset 0 0 12px var(--color),
+      inset 0 0 24px var(--bg-color);
+  }
+  table {
+    width: 100%;
+  }
+
+  .box3 {
+    width: 80px;
+  }
+  button {
+    margin: 8px 0;
+  }
+  .toast {
+    position: fixed;
+    margin: auto;
+    text-align: center;
+    top: 6vh;
+    width: 38.2vw;
+    min-width: 100px;
+    background-color: var(--bg-color);
+    color: var(--color);
+    padding: 10px 20px;
+    border-radius: 5px;
+    box-shadow:
+      1px 1px 24px var(--color),
+      1px 1px 12px var(--bg-color);
+  }
+</style>
